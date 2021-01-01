968ec2073759be3042b1f924e71fca84873e67b1
iskender <iskender@iskender-MacBook-Air.local>
2021-01-01 14:32:55 +0300

    test-stage3.0

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,6 +6,6 @@ echo "*******************************"
 
 package=mvn -B -DskipTests clean package
 test=mvn test
-WORKSPACE=/var/jenkins/workspace/macos-pipeline
+WORKSPACE=/Users/iskender/Desktop/volumes/jenkins/workspace/macos-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /var/jenkins/pomdependencies:/root/.m2/ -w /app maven:3-alpine "$@"
