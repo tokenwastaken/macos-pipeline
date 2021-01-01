89d7540f5bee4bd9fe8085ce0821b32c4779b8f1
iskender <iskender@iskender-MacBook-Air.local>
2021-01-01 16:44:06 +0300

    final

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -20,4 +20,4 @@ docker push wizzyy/$IMAGE:$BUILD_TAG
 
 echo $IMAGE > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
-scp /tmp/.auth iskender@192.168.0.16:/Users/iskender/Desktop/pipeline/push/auth
+scp /tmp/.auth iskender@192.168.0.16:/Users/iskender/Desktop/pipeline/jenkins/push/auth
