28c65ac43c81d328df0ef9923ce15643f9f428d1
iskender <iskender@iskender-MacBook-Air.local>
2021-01-01 14:35:55 +0300

    test-stage4.0

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,6 +8,6 @@ echo "*******************************"
 package=mvn -B -DskipTests clean package
 WORKSPACE=/Users/iskender/Desktop/volumes/jenkins/workspace/macos-pipeline
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /var/jenkins/pomdependencies:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /Users/iskender/Desktop/volumes/jenkins/pomdependencies:/root/.m2 -w /app maven:3-alpine "$@"
 
 #docker run --rm  -v  $WORKSPACE/java-app:/app   -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -8,4 +8,4 @@ package=mvn -B -DskipTests clean package
 test=mvn test
 WORKSPACE=/Users/iskender/Desktop/volumes/jenkins/workspace/macos-pipeline
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /var/jenkins/pomdependencies:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /Users/iskender/Desktop/volumes/jenkins/pomdependencies:/root/.m2/ -w /app maven:3-alpine "$@"
