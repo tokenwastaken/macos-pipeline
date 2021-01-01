344b23660a0cc98aba29740efc87a899e7a5e589
iskender <iskender@iskender-MacBook-Air.local>
2021-01-01 13:28:07 +0300

    build-stage7

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,6 +8,6 @@ echo "*******************************"
 package=mvn -B -DskipTests clean package
 WORKSPACE=/var/jenkins_home/workspace/macos-pipeline
 
-#docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
-docker run --rm  -v  $WORKSPACE/java-app:/app   -w /app maven:3-alpine "$@"
+#docker run --rm  -v  $WORKSPACE/java-app:/app   -w /app maven:3-alpine "$@"
