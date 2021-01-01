2b3066fd854acb36d21832120b2fd383c00b8bb3
iskender <iskender@iskender-MacBook-Air.local>
2021-01-01 17:41:04 +0300

    final

--- jenkins/push/push-k8s.sh
+++ jenkins/push/push-k8s.sh
@@ -5,7 +5,7 @@
 #after pushing the image to dockerhub we ssh into our workernode which is in this case the mac host that containers run on and we deploy pods and deployments which our main app runs on to minikube 
 export IMAGE=$(sed -n '1p' /Users/iskender/Desktop/pipeline/jenkins/push/auth)
 export BUILD_TAG=$(sed -n '2p' /Users/iskender/Desktop/pipeline/jenkins/push/auth)
-kubectl create -f $PWD/Desktop/pipeline/jenkins/push/maven-pod.yml
-kubectl create -f $PWD/Desktop/pipeline/jenkins/push/maven-deploy.yml
+kubectl create -f /Users/iskender/Desktop/pipeline/jenkins/push/maven-pod.yml
+kubectl create -f /Users/iskender/Desktop/pipeline/jenkins/push/maven-deploy.yml
 
 
